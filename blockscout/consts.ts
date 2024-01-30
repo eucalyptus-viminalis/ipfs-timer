4270e6654b884891306e2c0140a208021c9c5a55
eucalyptus-viminalis <fttx5525@gmail.com>
2024-01-30 22:39:08 +1100

    lint

--- blockscout/consts.ts
+++ blockscout/consts.ts
@@ -1,2 +1,1 @@
 export const ZORA_API_URL = "https://explorer.zora.energy/api/v2"
-export const ZAIBS_CA = "0x040cabddc5c1ed83b66e0126e74e7f97e6ec36bc"
--- viem/contract.ts
+++
@@ -1,4 +0,0 @@
-import { getContract } from "viem";
-import { erc1155abi } from "./abi";
-import { ZAIB_CONTRACT_ADDRESS } from "./consts";
-import { publicClient } from "./client";
